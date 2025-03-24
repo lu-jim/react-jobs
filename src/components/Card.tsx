0f0f5b7c1609683c788a20227b7e210f61ea822c
Luis Fernando Jimenez <lu.jim@protonmail.com>
2025-03-24 22:13:00 +0100

    card component and homecards wrapper

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,26 @@
-const Card = () => {
+interface CardProps {
+  card_name: string;
+  tag: string;
+  cta: string;
+  ctaLink: string;
+}
+
+
+
+const Card = ({ card_name, tag, cta, ctaLink }: CardProps) => {
   return (
-    <div>Card</div>
+    <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
+      <h2 className="text-2xl font-bold">{card_name}</h2>
+      <p className="mt-2 mb-4">
+        {tag}
+      </p>
+      <a
+        href={ctaLink}
+        className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+      >
+        {cta}
+      </a>
+    </div>
   )
 }
 
--- src/components/HomeCards.tsx
+++ src/components/HomeCards.tsx
@@ -2,11 +2,26 @@ import Card from "./Card"
 
 const HomeCards = () => {
   return (
-    <>
-      <Card />
-      <Card />
-      <Card />
-    </>
+    <section className="py-4">
+      <div className="container-xl lg:container m-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+
+          <Card
+            card_name="For Developers"
+            tag="Browse our React jobs and start your career today"
+            cta="Browse Jobs"
+            ctaLink="/jobs.html"
+          />
+          <Card
+            card_name="For Employers"
+            tag="List your job to find the perfect developer for the role"
+            cta="Add Job"
+            ctaLink="/add-job.html"
+          />
+        </div>
+      </div>
+    </section>
+
   )
 }
 
